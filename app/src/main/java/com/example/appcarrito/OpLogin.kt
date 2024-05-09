c2f43fea3b198692929e9a3334aec267fd43fee0
anamacam <anamacam.am@gmail.com>
2024-05-09 16:46:29 -0500

    Diseño la UI del usuario registrado

--- app/src/main/java/com/example/appcarrito/OpLogin.kt
+++ app/src/main/java/com/example/appcarrito/OpLogin.kt
@@ -5,16 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appcarrito.databinding.ActivityMainBinding
+import com.example.appcarrito.databinding.ActivityOpLoginBinding
 
 class OpLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOpLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_op_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+       binding = ActivityOpLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
---
+++ app/src/main/java/com/example/appcarrito/Opt_login/Login_correo.kt
@@ -0,0 +1,21 @@
+package com.example.appcarrito.Opt_login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appcarrito.R
+
+class Login_correo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_correo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
